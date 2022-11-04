03766fe94e1d7272079e37013bfd702ae36395ed
Myltiane <myltiane.aux@gmail.com>
2022-11-04 12:35:22 -0300

    feat: finaliazando schedule item aula 07

--- pages/schedules.tsx
+++ pages/schedules.tsx
@@ -2,7 +2,7 @@ import { format } from "date-fns";
 import locale from "date-fns/locale/pt-BR";
 import { get } from "lodash";
 import { NextPage, Redirect } from "next";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { useForm } from "react-hook-form";
 import Header from "../components/Header";
 import Page from "../components/Page";
@@ -13,6 +13,8 @@ import { useRouter } from "next/router";
 import { withAuthentication } from "../utils/withAuthentication";
 import { ScheduleSession } from "../types/ScheduleSession";
 import { Schedule } from "../types/Schedule";
+import { redirectToAuth } from "../utils/redirectToAuth";
+import Title from "../components/Page/Title";
 
 
 type FormData = {
@@ -20,62 +22,56 @@ type FormData = {
 }
 
 type ComponentPageProps = {
-    schedule: ScheduleSession;
-    data: Schedule;
-
+    schedules: Schedule[];
+    token: string;
 }
 
-const ComponentPage: NextPage<ComponentPageProps> = ({ schedule, data, }) => {
+const ComponentPage: NextPage<ComponentPageProps> = ({ schedules, token, }) => {
 
-    const {
-        handleSubmit,
-        setError,
-        formState: { errors },
-        clearErrors,
-    } = useForm<FormData>();
+    const [nextSchedules, setNextSchedules] = useState<Schedule[]>(schedules.filter(s => new Date(s.scheduleAt).getTime() > new Date().getTime()));
+    const [historySchedules, setHistorySchedules] = useState<Schedule[]>(schedules.filter(s => new Date(s.scheduleAt).getTime() <= new Date().getTime()));
 
-    const router = useRouter();
+    const onCanceled = () => {
+
+        axios.get<Schedule[]>(`/schedules`, {
+            baseURL: process.env.API_URL,
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }).then(({ data }) => {
+
+            setNextSchedules(data.filter(s => new Date(s.scheduleAt).getTime() > new Date().getTime()));
+            setHistorySchedules(data.filter(s => new Date(s.scheduleAt).getTime() <= new Date().getTime()));
+
+        }).catch(console.error);
+
+    }
 
     return (
         <Fragment>
             <Header />
             <Page
                 pageColor="green"
-                title={
-                    <Fragment>
-                         <h1>Pagamento Efetuado<small>Confira os detalhes do pedido</small></h1>
-                    </Fragment>
-                }
-                id="schedules-cmplete"
+                title="Agendamentos"
+                id="schedules"
             >
-                 <header className="page-title">
-                    <h2>Obrigado!</h2>
-                    <hr />
-                </header>
+                 <ul>
+                    {nextSchedules.length === 0 && <li> Não há póximos agendamentos.</li>}
+                    {nextSchedules.map((schedule, index) => <ScheduleItem key={index}  token={token} schedule={schedule} onCanceled={onCanceled} />)}
+                 </ul>
 
-                <p>{format(
-                    new Date(data.scheduleAt),
-                    "d 'de' MMMM 'de' yyyy",
-                    { locale }
-                )}</p>
+                <Title value="Histórico"/>
                 <p>
                     Número do Pedido: {String(data.id).padStart(6, "0")}<small>Cartão de Crédito final {schedule.cardLastFourDigits}</small>
                 </p>
 
-                <Toast
-                    type='danger'
-                    open={Object.keys(errors).length > 0}
-                    onClose={() => clearErrors()}
-                >
-                    {Object.keys(errors).map((err) => (
-                        get(errors, `${err}.message`, 'Verifique os serviços selecionados.')
-                    ))}
-                </Toast>
+
                 <Footer
                     buttons={[
                         {
-                            value: "Agendamentos",
-                            href: "/schedules",
+                            value: "Novo Agendamento",
+                            href: "/schedules-new",
+                            className:"black"
                         }
                     ]}
                 />
@@ -86,13 +82,13 @@ const ComponentPage: NextPage<ComponentPageProps> = ({ schedule, data, }) => {
 
 export default ComponentPage;
 
-export const getServerSideProps = withAuthentication(async ({ req }) => {
+export const getServerSideProps = withAuthentication(async (context) => {
 
     try {
 
-        const { schedule, token } = req.session;
+        const { token } = context.req.session;
 
-        const { data } = await axios.get(`/schedules/${req.session.schedule.data?.id}`, {
+        const { data: schedules } = await axios.get(`/schedules`, {
             baseURL: process.env.API_URL,
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -101,17 +97,13 @@ export const getServerSideProps = withAuthentication(async ({ req }) => {
 
         return {
             props: {
-                schedule,
-                data,
+                schedules,
+                token,
             },
         };
 
     } catch(e) {
-        return {
-            redirect: {
-                destination: '/schedules-summary',
-            } as Redirect,
-        }
+        return redirectToAuth(context)
     }
 
 });
--- types/Schedule.ts
+++ types/Schedule.ts
@@ -1,22 +1,33 @@
-// import { Address } from "./Address";
+import { Address } from "./Address";
 import { PaymentSituation } from "./PaymentSituation";
-// import { Person } from "./Person";
+import { Person } from "./Person";
 import { ScheduleService } from "./ScheduleService";
 import { TimeOption } from "./TimeOption";
 
 export type Schedule = {
     id: number;
-    personId: number;
-    timeOptionId: number;
-    PaymentSituationId: number;
-    billingAddressId: number;
-    scheduleAt: string;
-    total: number;
-    installments: number;
-    document: string;
-    paymentMethod: string;
-    cardToken: string;
-    ScheduleService: ScheduleService[];
 
+	personId: number;
+	person?: Person;
+
+	timeOptionId: number;
+	timeOption?: TimeOption;
+
+	paymentSituationId: number;
+	paymentSituation?: PaymentSituation;
+
+	billingAddressId: number;
+	billingAddress?: Address;
+
+	scheduleAt: string;
+	total: string;
+	installments: number;
+	document: string;
+	paymentMethod: string;
+	cardToken: string;
+
+	ScheduleService?: ScheduleService[];
+
+	address?: Address;
 
 }
